33c0e0f31b8ee4d73ab9998c713efbb88ee1eac7
Ali Alwash <alitrix@gmail.com>
2020-10-21 13:39:26 +0200

    Typo in example

--- examples/ask-bid-price.js
+++ examples/ask-bid-price.js
@@ -11,6 +11,7 @@ const degiro = DeGiro.create({
 
 const id = '350009261'; // you can get this id by using searchProduct()
 
+
 degiro.login().then(() => degiro.getAskBidPrice(id))
     .then(res => console.log(JSON.stringify(res, null, '  ')))
     .catch(console.error);
